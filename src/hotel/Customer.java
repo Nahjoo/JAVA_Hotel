37884c4e70899eb757625d58b2f4fecc85f6c404
Nahjoo <johan-delcourt62@hotmail.fr>
2019-09-29 17:57:38 +0200

    fix problem , reverse code Owner to customer and customer to Owner

--- src/hotel/Customer.java
+++ src/hotel/Customer.java
@@ -4,11 +4,34 @@ import java.time.LocalDate;
 
 public class Customer extends People {
 	
+	private String email;
+	private String phoneNumber;
+	
 	public Customer() {
 		super();
+		email = "Inconnu";
+		phoneNumber = "Inconnu";
 	}
 	
-	public Customer(String lastname, String firstname, LocalDate birthday, String adress) {
+	public Customer(String lastname, String firstname, LocalDate birthday, String adress, String email, String phoneNumber) {
 		super(lastname,firstname,birthday,adress);
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 }
